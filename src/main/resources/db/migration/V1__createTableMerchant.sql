28b60471f2edda3963a8e42696129292173f4f05
Borba <gabriel.lewis@acad.pucrs.bbr>
2021-03-23 18:14:21 -0300

    Initial Setup

---
+++ src/main/resources/db/migration/V1__createTableMerchant.sql
@@ -0,0 +1,3 @@
+CREATE TABLE if not exists application_user (
+
+);
---
+++ src/test/java/rpg/dungeon/DungeonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rpg.dungeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
